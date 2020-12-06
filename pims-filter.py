dc37bde43639e29421270b21e4441b6e46dbbc44
Rico Picone <ricopicone@gmail.com>
2020-12-06 15:36:07 -0800

    updates

--- pims-filter.py
+++ pims-filter.py
@@ -4,11 +4,12 @@ from scipy.linalg import eig, eigh
 from scipy.sparse.linalg import eigs, eigsh
 from scipy.sparse import dok_matrix, diags
 from matplotlib import pyplot as plt
+from matplotlib import rc
+import tikzplotlib
 
 plot_it = True # must be n_PC = 2
 n_PC = 2 # number of principle components
-adjacency_file = 'dialectica-pimsifier/adjacency.json'
-test_file = 'dialectica-pimsifier/THESIS-code-dump/test_data.json'
+adjacency_file = 'adjacency.json'
 
 # load adjacency
 with open(adjacency_file, "r") as read_file:
@@ -28,18 +29,25 @@ for i in range(0,n_nodes):
   for j in range(0,len(adjacents)):
     A[i,adjacents[j]] = 1
     A[adjacents[j],i] = 1
+print(f"  - A:\n{A}")
 # populate Laplacian
 L = D - A
+
+# print(f"  - Lapacian:\n{L.todense()}")
+
+# print(f"  - Lapacian eig:\n{eig(L.todense())[0]}")
 
 # eigendecomposition
 # - using ARPACK via https://docs.scipy.org/doc/scipy/reference/tutorial/arpack.html
 # - we want the largest eigenvals/vecs of L^+
 # - equivalently, we find the smallest eigenvals/vecs of L
 # - used sigma=0 to do the shift-invert mode
-evals_small, evecs_small = eigsh(L, n_PC, sigma=0, which='LM')
-# evals_small, evecs_small = eigsh(L, n_PC, which='SA')
-evals = 1/evals_small
-evecs = evecs_small # scale by whatever
+try:
+  evals_small, evecs_small = eigsh(L, n_PC+1, sigma=0, which='LM')
+except RuntimeError:
+  evals_small, evecs_small = eigsh(L, n_PC+1, which='SA')
+evals = 1/evals_small[1:None] # exclude smallest because it's approximating the zero eigenvalue
+evecs = evecs_small[:,1:None] # exclude smallest because it's approximating the zero eigenvalue
 evecs_T = evecs.T
 print(f"  - truncated evals: {evals}")
 
@@ -65,10 +73,31 @@ P = Lambda_root*U.T # sparse matrix mult
 P = P[0:n_PC,:]
 print(f"  - P shape: {P.shape}")
 
-# plot
+print(f"  - P: {P.todense()}")
+
+# plot scatter 
 if plot_it:
   P = np.asarray(P.todense())
   plt.scatter(P[0,:],P[1,:])
   # plt.tight_layout()
   plt.xlim([P[0,:].min(),P[0,:].max()])
+  plt.show()
+
+# plot labeled scatter 
+if plot_it:
+  # rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
+  rc('text', usetex=True)
+  fig, ax = plt.subplots()
+  labels = ["\\bigcup","A_1","A_{12}","A_{13}","A_{123}","B_1","B_{12}","B_{13}","B_{134}","B_1 C_1","C_1","C_{12}","C_{13}","C_{134}"]
+  labels = ["$"+s+"$" for s in labels]
+  colors = ['black',"green","green","green","green","blue","blue","blue","blue","purple","red","red","red","red"]
+  for i in range(0,n_nodes):
+    ax.plot(P[0,i],P[1,i], marker='o',color=colors[i])
+    ax.text(P[0,i]+.02,P[1,i]+.02, 
+      labels[i], color='black', 
+      ha='left', va='bottom'
+    )
+  plt.xlim([-2,1])
+  plt.ylim([-1.5,1.5])
+  tikzplotlib.save("scatter_tikz.tex")
   plt.show()
